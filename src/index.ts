6f8100504d3762676dac11f6e29340b2ff4a743a
Patrick Hochstenbach <patrick.hochstenbach@ugent.be>
2023-11-06 14:10:22 +0100

    Adding a listener example

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,33 @@
-import { CharStream, CommonTokenStream }  from 'antlr4';
-import cool_langLexer  from './grammars/cool_langLexer';
-import cool_langParser  from './grammars/cool_langParser';
+import { CharStream, CommonTokenStream, ParseTreeWalker }  from 'antlr4';
+import mylangLexer from './grammars/mylangLexer';
+import mylangParser, { ExprContext, ProgContext } from './grammars/mylangParser';
+import mylangListener from './grammars/mylangListener';
 
-const input = `
-var one;
-var two;
-var three;
-three = one + two;
-if (two + two) then {
-  STOP
-} else {
-  if (one * one) then { CONTINUE }
+const input = `1+2`; 
+
+class MyTreeWalker extends mylangListener {
+
+  exitProg = (_ctx: ProgContext) => {
+      console.log("In MyStartRule");
+  };
+  
+  exitExpr = (ctx: ExprContext) => {
+      if (ctx.getChildCount() == 3) {
+        console.log(
+          `>>` + 
+          ctx.getChild(0).getText() + 
+          ctx.getChild(1).getText() +
+          ctx.getChild(2).getText()
+        );
+      }
+  }
 }
-`; 
-const chars = new CharStream(input); // replace this with a FileStream as required
-const lexer = new cool_langLexer(chars);
-const tokens = new CommonTokenStream(lexer);
-const parser = new cool_langParser(tokens);
-const tree = parser.file();
 
-console.log(tree.toStringTree(null, parser));
+const chars = new CharStream(input); // replace this with a FileStream as required
+const lexer = new mylangLexer(chars);
+const tokens = new CommonTokenStream(lexer);
+const parser = new mylangParser(tokens);
+const tree = parser.prog();
+
+const walker = new MyTreeWalker();
+ParseTreeWalker.DEFAULT.walk(walker, tree);
